42e92848d22bf83abf4184cdcff73bb1f1a9f2aa
Kyuunex <kyuunex@protonmail.ch>
2022-09-16 22:01:42 +0400

    add basic admin panel and sql console

---
+++ minbl/blueprints/administration.py
@@ -0,0 +1,59 @@
+"""
+This file provides endpoints for everything blog related
+"""
+from flask import Blueprint, request, redirect, url_for, render_template
+
+from minbl.reusables.context import db_cursor
+from minbl.reusables.context import db_connection
+from minbl.reusables.context import website_context
+from minbl.reusables.user_validation import get_user_context
+
+administration = Blueprint("administration", __name__)
+
+
+@administration.route('/administration')
+def admin():
+    user_context = get_user_context()
+    if not user_context:
+        return redirect(url_for("user_management.login_form"))
+    if not user_context.permissions >= 9:
+        return "you do not have permissions to perform this action"
+
+    return render_template("admin_panel.html", WEBSITE_CONTEXT=website_context, USER_CONTEXT=user_context)
+
+
+@administration.route('/administration/sql_form')
+def sql_form():
+    user_context = get_user_context()
+    if not user_context:
+        return redirect(url_for("user_management.login_form"))
+    if not user_context.permissions >= 9:
+        return "you do not have permissions to perform this action"
+
+    return render_template(
+        "sql.html",
+        WEBSITE_CONTEXT=website_context,
+        USER_CONTEXT=user_context
+    )
+
+
+@administration.route('/administration/sql_exec', methods=['POST'])
+def sql_exec():
+    user_context = get_user_context()
+    if not user_context:
+        return redirect(url_for("user_management.login_form"))
+    if not user_context.permissions >= 9:
+        return "you do not have permissions to perform this action"
+
+    if request.method == 'POST':
+        sql_query = request.form['sql_query']
+
+        sql_results = tuple(db_cursor.execute(sql_query))
+        db_connection.commit()
+
+        return render_template(
+            "sql.html",
+            WEBSITE_CONTEXT=website_context,
+            USER_CONTEXT=user_context,
+            SQL_RESULTS=sql_results
+        )
