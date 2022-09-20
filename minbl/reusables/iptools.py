5fbd3a3fde37662bd5479ccfeb0044823e5b0ff6
Kyuunex <kyuunex@protonmail.ch>
2022-09-21 03:49:45 +0400

    add cloudflare support

--- minbl/reusables/iptools.py
+++ minbl/reusables/iptools.py
@@ -2,7 +2,11 @@ import ipaddress
 
 
 def ip_decode(request):
-    client_ip_address_str = request.remote_addr
+    if request.headers.get("CF-Connecting-IP"):
+        client_ip_address_str = request.headers.get("CF-Connecting-IP")
+    else:
+        client_ip_address_str = request.remote_addr
+
     if "." in client_ip_address_str:
         return 0, ipaddress.IPv4Address(client_ip_address_str)
     elif ":" in client_ip_address_str:
