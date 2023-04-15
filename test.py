17275965fa8e13805e31bcec2d1bb7e1a13f6f36
Kyuunex <kyuunex@protonmail.ch>
2023-04-15 17:45:06 +0400

    change test.py default stuff

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 import os
 
-os.environ["MINBL_SQLITE_FILE"] = os.path.expanduser("~") + "/.minbl.sqlite3"
+os.environ["MINBL_SQLITE_FILE"] = os.path.expanduser("~") + "/Documents/minbl.sqlite3"
 
 from minbl import app as application
 
 application.run(
     host='127.0.0.1',
-    port=8080,
+    port=8088,
     debug=True
 )
