330e12ee765d55f83de08e358f36f04e48b27da7
Kyuunex <kyuunex@protonmail.ch>
2023-08-11 19:44:50 +0400

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     ],
     include_package_data=True,
     package_data={'minbl': ['static/*', 'templates/*']},
-    version="0.7.5",
+    version="0.7.6",
     description='A minimalist blog',
     author='Kyuunex',
     author_email='kyuunex@protonmail.ch',
